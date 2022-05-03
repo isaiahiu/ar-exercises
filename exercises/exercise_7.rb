2543de2cb309cc7d8b084edc988d2ffd28e6dd1c
Isaiah <isaiah.iu@hotmail.com>
2022-05-03 19:47:15 +0000

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -8,5 +8,36 @@ require_relative './exercise_6'
 
 puts "Exercise 7"
 puts "----------"
+# 1. Add validations to two models to enforce the following business rules:
+
+# - Employees must always have a first name present
+# - Employees must always have a last name present
+# - Employees have a hourly_rate that is a number (integer) between 40 and 200
+# - Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+# - Stores must always have a name that is a minimum of 3 characters
+# - Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# - BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
+
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
 
 # Your code goes here ...
+
+
+print "What would you like the store name to be?:  "
+name = gets.chomp
+
+newStore = Store.create(name: name)
+newStore.errors.full_messages.each do |message| puts "#{message}" end
+  
+# TESTING BELOW 
+
+
+  # puts newStore.valid?
+  # p newStore.errors
+
+
+# puts @store1.employees.create(first_name: "Test", last_name: "Case", hourly_rate: 45).valid?
+# random = Employee.create(first_name: "Franky", last_name: "Joe", hourly_rate: 55, store_id: 22 )
+# puts random.valid?
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
-has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :clothes?
+  has_many :employees
+
+  def clothes?
+    # if !(mens_apparel || womens_apparel) 
+    #   errors.add(:mens_apparel, :womens_apparel, message: "Needs to sell one of these!")
+    # end
+    errors.add(:mens_apparel, "Needs to sell either one of these!") unless (mens_apparel || womens_apparel)
+  end
 end
+
